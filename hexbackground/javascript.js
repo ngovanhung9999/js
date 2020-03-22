06ce3053524f914e3dce36e4b00735c3e343ec29
ngovanhung9999 <nhokkoro101095@gmail.com>
2020-03-22 22:08:53 +0700

    them 2 project count and hex

---
+++ hexbackground/javascript.js
@@ -0,0 +1,16 @@
+(function() {
+    let button = document.getElementById("btn");
+    let span = document.getElementById("hexColor");
+    const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+    button.addEventListener("click", changeColor);
+
+    function changeColor() {
+        let hex = "#";
+        for (let i = 0; i < 6; i++) {
+            hex += hexValues[Math.floor(Math.random() * hexValues.length)];
+        }
+        span.innerHTML = hex;
+        document.body.style.backgroundColor = hex;
+    }
+    changeColor();
+})()
